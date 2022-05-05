154b2b32121910d2dde67cbffaa07794a439a589
mihailkokoulin <kokoulin92@gmail.com>
2022-05-05 20:39:19 +0400

    Added gocurrencies lib to project

--- cmd/exchanger/main.go
+++ cmd/exchanger/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	gocurrencies "github.com/KokoulinM/gocurrencies"
 	"github.com/KokoulinM/telegram-exchange-bot/cmd/exchanger/configs"
 	"github.com/KokoulinM/telegram-exchange-bot/internal/app/client"
 )
@@ -12,7 +13,7 @@ func main() {
 
 	c := client.New(cfg.BaseURL, cfg.ApiKey)
 
-	data, err := c.Convert("AMD", "RUB", "5000")
+	data, err := c.Convert(gocurrencies.Armenia, gocurrencies.RussianFederation, "5000")
 	if err != nil {
 		fmt.Println(err)
 	}
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/KokoulinM/telegram-exchange-bot
-
-go 1.18
-
-require github.com/caarlos0/env/v6 v6.9.1
